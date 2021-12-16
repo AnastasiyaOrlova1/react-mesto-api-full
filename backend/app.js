ba846b6b7546d00127f3e70efcff9134e7198356
Anastasiya Orlova <orlovastas@gmail.com>
2021-12-16 18:06:59 +0300

    fixies

--- backend/app.js
+++ backend/app.js
@@ -31,54 +31,9 @@ app.use(cors({
   origin: "https://mesto.orlovas.nomoredomains.rocks",
   credentials: true,
 }));
-/*const corsOptions ={
-  origin:'*',
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions));*/
-
-/*const allowedCors = [
-  'https://mesto.orlovas.nomoredomains.rocks/',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-
-  next();
-});*/
-
 
 app.use(requestLogger);
-/*app.use(cors());*/
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
